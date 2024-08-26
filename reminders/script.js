bd07a04a0844bbef194a95dd6e4e624ea60d7e35
Dlurak <84224239+Dlurak@users.noreply.github.com>
2024-08-26 21:20:00 +0200

    Update the reminder script

--- reminders/script.js
+++ reminders/script.js
@@ -1,50 +1,57 @@
-const school = 'Hogwarts';
-const className = '1a';
+////////////////////////
+// SETTINGS FOR USERS //
+////////////////////////
 
-const url = `https://dlool-backend.onrender.com/homework?page=1&pageSize=10&school=${school}&class=${className}`;
+const school = 'Hogwarts';
+const classes = [
+	'Programming',
+  'Haskell',
+];
+const reminderName = 'Dlool-homework'
+/** There is a warning that the id may not be edited, disable/enable this warning */
+const enableWarningDescriptionWarning = true
+
+//////////////////
+// SETTINGS END //
+//////////////////
+
+const url = `https://dlool.onrender.com/assignments?offset=0&limit=10&school=${school}&classes=${classes.join(',')}`;
 const req = await new Request(url).loadJSON();
 if (req.status !== 'success') return;
+const { assignments }  = req.data;
 
-const data = req.data.homework;
-
-
-const cal = await Calendar.findOrCreateForReminders('Dlool - Homework')
-
-const createDate = (date) => new Date(date.year, date.month - 1, date.day)
-
-const createReminder = (assign, id, index, reminder = new Reminder()) => {
-  reminder.title = assign.subject;
-  reminder.notes = `${assign.description}\n\nDo not edit the id!\n${id}+${index}`;
-  reminder.calendar = cal;
-  reminder.dueDate = createDate(assign.due)
-  reminder.dueDateIncludesTime = false;
-  
-  return reminder;
-}
-
+const cal = await Calendar.findOrCreateForReminders(reminderName);
 
 const allReminder = await Reminder.all([cal]);
 
-const getRemindersById = (reminders, id, index) => {
-  return reminders.filter((rem) => {
-    const match = rem.notes.endsWith(`${id}+${index}`)
-    return match;
-  })
-};
+const createDate = (date) => new Date(date.year, date.month - 1, date.day);
+
+const createReminder = (assign, id, reminder = new Reminder()) => {
+	reminder.title = assign.subject;
+	reminder.notes = enableWarningDescriptionWarning
+		? `${assign.description}\n\nDo not edit the id!\n${id}`
+		: `${assign.description}\n\n${id}`;
+	reminder.calendar = cal;
+	reminder.dueDate = createDate(assign.due)
+	reminder.dueDateIncludesTime = false;
+
+	return reminder;
+}
+
+const getRemindersById = (reminders, id) => 
+	reminders.filter((rem) => 
+		rem.notes?.endsWith(id)
+	)
 
 
-data.forEach((hw) => {
-  let index = 0;
-  
-  hw.assignments.forEach((assign) => {
-    const r = getRemindersById(allReminder, hw._id, index);
-    const reminderExists = r.length >= 1;
-    
-    const reminder = reminderExists
-        ? createReminder(assign, hw._id, index, r[0])
-        : createReminder(assign, hw._id, index);
-        
-    reminder.save()
-    index++;
-  });
+assignments.forEach((assign) => {
+	const { id } = assign
+	const r = getRemindersById(allReminder, id);
+	const reminderExists = r.length >= 1;
+
+	const reminder = reminderExists
+		? createReminder(assign, id, r[0])
+		: createReminder(assign, id);
+
+	reminder.save()
 });
